97a0589924ac22fef2116e759cc5814e67923b9c
leontoys <svliyon@gmail.com>
2026-04-25 05:42:32 +1000

    changed tag

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! Fixed github actions</h1>')
+  res.send('<h1>Hello World! Changed tag</h1>')
 })
 
 const PORT = 8080
