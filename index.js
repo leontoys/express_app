bde8c270ad26273df945f8190230f99b1b4cdbc9
leontoys <svliyon@gmail.com>
2026-04-25 06:09:08 +1000

    pipeline is set up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! Watch tower is running</h1>')
+  res.send('<h1>Hello World! Pipeline is set up</h1>')
 })
 
 const PORT = 8080
